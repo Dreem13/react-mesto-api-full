60629c28bce93166d7dc87397ff24f76e44849ba
Dmitry Larionov <d.larionoff@yandex.ru>
2021-08-28 17:49:55 +0300

    add crash test

--- backend/app.js
+++ backend/app.js
@@ -62,6 +62,12 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', validateSignUp, createUser);
 app.post('/signin', validateSignIn, login);
 
